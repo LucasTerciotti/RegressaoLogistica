7f0dc3dfbbea05d4a7ab35854a8ab56224bc8e07
LucasTerciotti <lucasterciotti@gmail.com>
2018-06-17 23:21:31 -0300

    Analise: previsão de nascimento de crianças com baixo peso.

--- birth_control.R
+++ birth_control.R
@@ -5,6 +5,9 @@ bw <- read.csv(("https://raw.githubusercontent.com/Efsilvaa/EPSMLRepo/master/Dat
 library(randomForest)
 library(rpart)
 library(Amelia)
+library(rpart.plot)
+library(RColorBrewer)
+library(rattle)
 
 missmap(bw, main = "Missing values vs observed")
 summary(bw)
@@ -29,11 +32,36 @@ fit <- glm(low ~ age + lwt + race +
            data = train,
            family = binomial(link = "logit"))
 summary(fit)
+#aqui podemos verificar que age, lwt e ftv apresentam um alto valor de P-value, o que os sinaliza
+#como estatisticamente insignificantes.
+anova(fit, test="Chisq")
+#Aqui reforçamos a ideia de que algumas variáveis não contribuem significativamente para o modelo, ja que
+#ao serem adicionadas a queda no desvio residual é baixa em comparação a contribuição de, por exemplo, 
+#smoke, ptl e ui. Um p-value alto aqui indica que o modelo explicaria mais ou menos a mesma quantidade de
+#variação sem a adição dessas variaveis (race, age, ftv - as duas ultimas tambem foram vistas  summary)
 
+#fizemos o modelo com o conjunto "train" de dados, agora aplicaremos em "test":
+prediction_prob <- predict(fit,newdata=test,type='response')
+prediction <- ifelse(prediction_prob > 0.5,1,0)
+table(prediction,test$low)
+prop.table(table(prediction,test$low))
+
+#
+#prediction          0          1
+#         0 0.59649123 0.28070175
+#         1 0.05263158 0.07017544
+#Alcançamos uma acurácia de aproximadamente 66,6% de acerto. Devido as analises feitas anteriormente,
+#vamos tentar utilizar um conjunto de dados menor: retiraremos os menos relevantes tanto pela análise em
+#Summary quanto pela Anova:
+fit <- glm(low ~ lwt + race +
+             smoke + ptl + ht + ui,
+           data = train,
+           family = binomial(link = "logit"))
+summary(fit) #ainda verificamos lwt com alto P-value
 anova(fit, test="Chisq")
 
 prediction_prob <- predict(fit,newdata=test,type='response')
 prediction <- ifelse(prediction_prob > 0.5,1,0)
 table(prediction,test$low)
-
 prop.table(table(prediction,test$low))
+# agora alcançamos exatamente a mesma acurácia anterior (66,6%), mesmo tendo retirado age e ftv.
